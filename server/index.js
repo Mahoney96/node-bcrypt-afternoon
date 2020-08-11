7c6c34ff86b3c739c5f0c88ef9be34c05d0888d6
Mahoney96 <cmahoney966@gmail.com>
2020-08-11 15:32:07 -0700

    3:32pm

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+// const authController = require('./controllers')
+
+const port = 3993;
+
+const {SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+const app = express()
+
+app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnathorized: false }
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+});
+
+app.listen(PORT, ()=>console.log(`Hey Hey Your Port Works Today! on ${PORT}`))
